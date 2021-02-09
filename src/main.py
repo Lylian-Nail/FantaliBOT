fca1c403ec58dac941caca550016a8bc64fbc286
Lylian Person-Gay <lylian.person-gay_dev@protonmail.com>
2021-02-09 20:44:22 +0100

    Place the discord token on ENV DISCORD_TOKEN

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from commands import misc,capitals
+import os
 
 bot = commands.Bot(
     command_prefix='$'
@@ -8,4 +9,4 @@ bot = commands.Bot(
 bot.add_cog(misc.Misc(bot))
 bot.add_cog(capitals.Capitals.LoadFromCSV(bot, 'src/datas/capitales.csv'))
 
-bot.run('ODA4NzI2NDk1MzI1NDU0Mzc2.YCKvPQ.Nv_yVSMuf4eyJR0heGlbAm-cGpw')
+bot.run(os.getenv("DISCORD_TOKEN"))
