dbfa1810d04ba513f1250285d06f0336f3ac426a
Lylian Person-Gay <lylian.person-gay_dev@protonmail.com>
2021-02-09 20:34:38 +0100

    Create a game of guessing capital of country 4 fun

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+from discord.ext import commands
+from commands import misc,capitals
 
+bot = commands.Bot(
+    command_prefix='$'
+)
 
-print("Hello World!")
+bot.add_cog(misc.Misc(bot))
+bot.add_cog(capitals.Capitals.LoadFromCSV(bot, 'src/datas/capitales.csv'))
+
+bot.run('ODA4NzI2NDk1MzI1NDU0Mzc2.YCKvPQ.Nv_yVSMuf4eyJR0heGlbAm-cGpw')
