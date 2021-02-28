b0d6d10497fd556b9fb5dd030d399e0f516d140b
Lylian Person-Gay <lylian.person-gay_dev@protonmail.com>
2021-02-28 20:39:59 +0100

    Fix inline-comment-prefix for ini files

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,6 @@
 import configparser
 
 def config(filename='config/config.ini'):
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(inline_comment_prefixes=(';', '#'))
     config.read(filename)
     return config
