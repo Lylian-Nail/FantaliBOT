723535eb4bde8c5d03b4e45c4ef179c701ec4f83
Lylian Person-Gay <lylian.person-gay_dev@protonmail.com>
2021-02-28 13:24:27 +0100

    Add config file for discord token etc

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from discord.ext import commands
 from commands import misc,geography
-import os
+from config.config import config
 
 bot = commands.Bot(
     command_prefix='$'
 )
 
-bot.add_cog(misc.Misc(bot))
+# Load conf from 'config/config.ini'
+config = config()
 
+bot.add_cog(misc.Misc(bot))
 bot.add_cog(geography.Geography(bot, 'src/datas/capitales.csv', 'src/datas/drapeaux.csv'))
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+bot.run(config['discord']['token'])
